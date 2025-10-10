70278a3b2eea4996a4bcd80b1c1e1e6e25cc097a
evan-ryan-york <evan.ryan.york@gmail.com>
2025-10-10 18:41:55 -0300

    Initial Commit

---
+++ Eko/EkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EkoApp.swift
+//  Eko
+//
+//  Created by Ryan York on 10/10/25.
+//
+
+import SwiftUI
+
+@main
+struct EkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
